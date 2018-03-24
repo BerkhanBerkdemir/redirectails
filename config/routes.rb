70daa4027f74a4c72bd34a0fdbcf95c99bad34e2
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-03-24 16:21:19 -0700

    Added delete method for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'landings#index'
 
+  # Admin dashbaoard routes
   get '/admin', to: 'admin#dashboard', as: 'admin'
+  delete '/admin/:id', to: 'admin#destroy', as: :delete
 
+  # Public routes
   get '/:url_id', to: 'url#redirect_user', as: 'redirect'
   get '/s/:url_id', to: 'url#show', as: 'settings'
   post '/', to: 'url#generate', as: 'generate'
+  
 end
