b00b5fb9797e05b12e50940034bd244f1ec1d0cc
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-03 03:56:35 -0700

    Added tests for URL field

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -14,6 +14,30 @@ RSpec.describe Url, type: :model do
     url = Url.new url: nil, url_id: 'J8kdSd', status: false
     expect(url).to_not be_valid
   end
+  
+  it 'is not valid if it is not start with http or https' do
+    url = Url.new url: 'ftp://example.com', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: 'http.example.com', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: 'http//example.com', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: 'https:/example.com', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+  end
+  
+  it 'is not valid if user add another type of data' do
+    url = Url.new url: 'lorem', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: '3', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: 3, url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: false, url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+    url = Url.new url: 'SELECT * FROM url', url_id: 'J8kdSd', status: false
+    expect(url).to_not be_valid
+  end
 
   it 'is not valid without url_id' do
     url = Url.new url: 'https://example.com', url_id: nil, status: false
