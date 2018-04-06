5db79b31393729d6bd98bc8e4b1f4dbbd95a89ff
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-06 05:42:54 -0700

    CodeFactoring

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,23 +1,24 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with name: ENV.fetch('NAME'), password: ENV.fetch('PASSWORD')
+  http_basic_authenticate_with name: ENV.fetch('NAME'),
+                               password: ENV.fetch('PASSWORD')
 
   # Get all URL from database
   def dashboard
     @url = Url.all
   end
-  
+
   # Delete query from database with :delete method
   def destroy
-
     url = Url.find(params[:id])
 
     # If it is successful return a success message with flash
     # If not return error message. Sysadmin should check error.
+    # TODO: Write I18n for 19 and 22.
     if url.destroy
-      flash[:success] = "Successfully deleted..."
+      flash[:success] = 'Successfully deleted...'
       redirect_to admin_path
     else
-      flash[:danger] = "Something went wrong. Please check your logs"
+      flash[:danger] = 'Something went wrong. Please check your logs'
       redirect_to admin_path
     end
   end
