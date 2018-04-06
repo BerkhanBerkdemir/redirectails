e3b1b191495938a2d738c64893d8fd50b64637c3
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-06 05:44:14 -0700

    CodeFactoring

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   get '/:url_id', to: 'url#redirect_user', as: 'redirect'
   get '/s/:url_id', to: 'url#show', as: 'settings'
   post '/', to: 'url#generate', as: 'generate'
-
 end
