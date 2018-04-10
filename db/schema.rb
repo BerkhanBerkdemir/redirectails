7237a61fa49d344df0845c2ddb0f858c0aeb2ccd
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-10 00:14:11 -0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_06_071345) do
+ActiveRecord::Schema.define(version: 20180410051643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_04_06_071345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "click", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
