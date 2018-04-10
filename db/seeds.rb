7b59ee092f7b55547354665597883bdb902bcf0a
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-10 00:15:17 -0700

    Seed created default admin profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
-# TODO: Create default admin user
+# Creates default admin user
+User.create(username: 'admin',
+            password: 'secretpassw0rd',
+            password_confirmation: 'secretpassw0rd',
+            role: 'ADMIN')
