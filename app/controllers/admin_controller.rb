84da667c4b4a0e53ec05ad04b846862f03803f8b
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-09 21:39:38 -0700

    Duplicated redirect_to removed

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -14,13 +14,11 @@ class AdminController < ApplicationController
     # If it is successful return a success message with flash
     # If not return error message. Sysadmin should check error.
     # TODO: Write I18n for 19 and 22.
-    # TODO: Remove one redirect_to and put another one into under the if condition
     if url.destroy
       flash[:success] = 'Successfully deleted...'
-      redirect_to admin_path
     else
       flash[:danger] = 'Something went wrong. Please check your logs'
-      redirect_to admin_path
     end
+    redirect_to admin_path
   end
 end
