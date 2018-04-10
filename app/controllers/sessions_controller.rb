0a5d8634afb0ecb8e6d6740b00d7058464dc1785
Berkhan Berkdemir <berkberkdemir@gmail.com>
2018-04-10 01:51:34 -0700

    Code style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :logged_in?, only: [:destroy]
+  before_action :authorize, only: [:destroy]
 
   def new
     # TODO: This is not good way to use.
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
+    user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
